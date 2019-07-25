fed1869e65172bbb8ccc573c4bc0739d90e53f47
deimi <deimi@gmx.at>
2019-07-25 07:08:56 +0200

    add if_constexpr example

---
+++ C++17/if_constexpr/main.cpp
@@ -0,0 +1,34 @@
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+int GetInt(){return 5;}
+float GetFloat(){return 12.345;}
+std::string GetString(){return std::string("asdf");}
+
+template<class T> struct dependent_false : std::false_type {};
+template <class ...> constexpr std::false_type always_false{};
+
+template<typename T>
+T GetValue(){
+    if constexpr (std::is_same<T, int>()){
+        return GetInt();
+    } else if constexpr (std::is_same<T, float>()){
+        return GetFloat();
+    } else if constexpr (std::is_same<T, std::string>()){
+        return GetString();
+    } else {
+        //static_assert(dependent_false<T>::value, "Type not supported");
+        static_assert(always_false<T>, "Type not supported");  // a direct static_assert(false, ...) is not possible because then the template would be ill-formed and won't compile for any type
+    }
+}
+
+int main()
+{
+    std::cout << "int " << GetValue<int>() << std::endl;
+    std::cout << "float " << GetValue<float>() << std::endl;
+    std::cout << "string " << GetValue<std::string>() << std::endl;
+    // std::cout << "uint " << GetValue<unsigned int>() << std::endl;  // This won't compile because it ends in the else path of GetValue
+}
+
