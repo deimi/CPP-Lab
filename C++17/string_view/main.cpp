c154d6dac6674ffe7427752baf535b65f14b14d8
deimi <deimi@gmx.at>
2019-07-30 18:44:03 +0200

    add string_view usage comparison

---
+++ C++17/string_view/main.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <string_view>
+
+//4     inline static const std::string test1 = "asdf";
+//4     inline static const std::string test2 = "qwer";
+
+int main() {
+//    constexpr std::string_view test1 = "asdf";
+//    constexpr std::string_view test2 = "qwer";
+//    std::string test = std::string(test1) + std::string(test2);       //programm code size 190
+
+//    std::string_view test1 = "asdf";
+//    std::string_view test2 = "qwer";
+//    std::string test = std::string(test1) + std::string(test2);         //programm code size 270
+
+//    static const std::string test1 = "asdf";
+//    static const std::string test2 = "qwer";
+//    std::string test = test1 + test2;               //programm code size 418
+
+//4    std::string test = test1 + test2;               //programm code size 399
+
+    constexpr std::string_view test1 = "asdf";
+    constexpr std::string_view test2 = "qwer";
+    std::string test(test1);
+    test.append(test2);             //programm code size 131
+  return 0;
+}
