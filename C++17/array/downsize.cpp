2511065c551daabbca5790c8fd234cf056c18441
deimi <deimi@gmx.at>
2019-08-01 00:25:57 +0200

    array(downsize): add compile time check dest smaller than source

--- C++17/array/downsize.cpp
+++ C++17/array/downsize.cpp
@@ -11,6 +11,7 @@ constexpr std::array<T, dest_size> DownsizeByOne(std::array<T, dest_size+1> othe
 
 template<typename T, int dest_size, int source_size, int current_pos=(dest_size-1), typename... Ts>
 constexpr std::array<T, dest_size> Downsize(std::array<T, source_size> other, Ts... rest){
+    static_assert(dest_size<source_size, "Destination not smaller than source");
     if constexpr(current_pos>0){
         return Downsize<T, dest_size, source_size, current_pos-1, Ts...>(other, other[current_pos], rest...);
     }
@@ -31,6 +32,7 @@ int main(){
     constexpr std::array<char, 4> test4 = Downsize<char,4,5>(test5);
     constexpr std::array<char, 3> test3 = Downsize<char,3,5>(test5);
     constexpr std::array<char, 2> test2 = Downsize<char,2,5>(test5);
+    //constexpr std::array<char, 5> test_failed = Downsize<char,5,5>(test5);      // compile time error because destination must be smaller than source
 
     // std::cout << "test5: " << test5[0] << test5[1] << test5[2] << test5[3] << test5[4] << std::endl;
     // std::cout << "test4: " << test4[0] << test4[1] << test4[2] << test4[3] << std::endl;
